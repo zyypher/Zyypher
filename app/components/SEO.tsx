eae3d4edee513a1dd9c8eba13cfec5c15b5d5c94
Vishnu Vinayan <therealvishnuvinayan@gmail.com>
2025-01-26 23:38:26 +0400

    added og

---
+++ app/components/SEO.tsx
@@ -0,0 +1,30 @@
+import Head from "next/head";
+
+const SEO = ({
+  title = "Zyypher - Your Technology Partner",
+  description = "Empowering businesses with top-notch technology solutions.",
+  url = "https://www.zyypher.com/",
+  image = "https://www.zyypher.com/images/mainLogo3.png",
+}) => {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+
+      {/* Open Graph / Facebook */}
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+      <meta property="og:url" content={url} />
+      <meta property="og:type" content="website" />
+
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
+    </Head>
+  );
+};
+
+export default SEO;
--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,17 @@ import HowItWorks from "./components/home/HowItWorks";
 import Introduction from "./components/home/Introduction";
 import Metrics from "./components/home/Metrics";
 import WhatYouGet from "./components/home/WhatWeGet";
+import SEO from "./components/SEO";
 
 export default function Home() {
   return (
     <>
+      <SEO
+        title="Zyypher - Innovative Technology Solutions"
+        description="Zyypher empowers businesses with cutting-edge technology solutions."
+        url="https://www.zyypher.com/"
+        image="https://www.zyypher.com/images/home-og-image.png"
+      />
       <Hero />
       <Introduction />
       <WhatYouGet />
